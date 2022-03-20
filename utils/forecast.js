ea89939d94296ecf3d4c4e654fb4f14e37b64ace
Asmita Kapadi <asmita.kapadi@ext.saint-gobain.com>
2022-03-20 11:09:51 +0530

    Updates

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,13 +1,13 @@
 const request = require('request')
 const forecast = (latitude,longitude,callback) => {
     const url = "http://api.weatherstack.com/current?access_key=624b54435945167952bc0f9eb714c150&query="+latitude+","+longitude+"&units=f"
-    request({ url: url, json:true}, (error, response) => {
+    request({ url: url, json:true}, (error, {body}) => {
         if(error){
             callback('Unable to connect with Location services',undefined)
-        }else if(response.body.error){
+        }else if(body.error){
             callback('Unable to connect with Location services. Try again.',undefined)
         }else{
-            callback(undefined,response.body.current.weather_descriptions[0] + ". The current temperature is "+response.body.current.temperature+" degrees, but it feels like it's "+response.body.current.feelslike+" degrees.")
+            callback(undefined,body.current.weather_descriptions[0] + ". The current temperature is "+ body.current.temperature+" degrees, but it feels like it's "+ body.current.feelslike+" degrees.")
         }
     })
 }
